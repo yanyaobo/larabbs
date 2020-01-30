4a0d051f31d83021c980e160657515788c3ec068
yanyaobo <506998345@qq.com>
2020-01-30 17:43:26 +0000

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
@@ -20,6 +21,6 @@ class UsersController extends Controller
     public function update(UserRequest $request, User $user)
     {
         $user->update($request->all());
-        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
